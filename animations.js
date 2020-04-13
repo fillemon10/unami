ad327f7b7641e9eb9fa9b921bec92288388e7a97
Sjolanderr <02filsjo@skola.boras.se>
2020-04-13 17:00:45 +0200

    i

--- animations.js
+++ animations.js
@@ -7,19 +7,34 @@
 
 //dropdown on hover
 window.addEventListener("load", function () {
-  const dropdown = document.querySelector(".dropdown");
-  const dropdownmenu = document.querySelector(".dropdown-menu");
-  const dropdowntoggle = document.querySelector(".dropdown-toggle");
+  const dropdown1 = document.querySelector(".dropdown1");
+  const dropdownmenu1 = document.querySelector(".dropdown-menu1");
+  const dropdowntoggle1 = document.querySelector(".dropdown-toggle1");
 
-  dropdown.addEventListener("mouseover", (event) => {
-    dropdown.classList.add("show");
-    dropdownmenu.classList.add("show");
-    dropdowntoggle.setAttribute("aria-expanded", "true");
+  dropdown1.addEventListener("mouseover", (event) => {
+    dropdown1.classList.add("show");
+    dropdownmenu1.classList.add("show");
+    dropdowntoggle1.setAttribute("aria-expanded", "true");
   });
 
-  dropdown.addEventListener("mouseout", (event) => {
-    dropdown.classList.remove("show");
-    dropdownmenu.classList.remove("show");
-    dropdowntoggle.setAttribute("aria-expanded", "false");
+  dropdown1.addEventListener("mouseout", (event) => {
+    dropdown1.classList.remove("show");
+    dropdownmenu1.classList.remove("show");
+    dropdowntoggle1.setAttribute("aria-expanded", "false");
+  });
+  const dropdown2 = document.querySelector(".dropdown2");
+  const dropdownmenu2 = document.querySelector(".dropdown-menu2");
+  const dropdowntoggle2 = document.querySelector(".dropdown-toggle2");
+
+  dropdown2.addEventListener("mouseover", (event) => {
+    dropdown2.classList.add("show");
+    dropdownmenu2.classList.add("show");
+    dropdowntoggle2.setAttribute("aria-expanded", "true");
+  });
+
+  dropdown2.addEventListener("mouseout", (event) => {
+    dropdown2.classList.remove("show");
+    dropdownmenu2.classList.remove("show");
+    dropdowntoggle2.setAttribute("aria-expanded", "false");
   });
 });
